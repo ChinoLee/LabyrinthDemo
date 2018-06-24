423ba60e18ce90f5e1fca374ee24febf2b37125d
ChinoLee <760349415@qq.com>
2018-06-24 09:41:15 +0800

    迷宫小游戏

---
+++ Assets/Scripts/CoinContorl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinContorl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.forward * 90 * Time.deltaTime);
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerContorl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContorl : MonoBehaviour {
+
+    public GameObject HitPre;
+    public GameObject BombPre;
+
+    private Rigidbody rBody;
+
+    public int hp = 3;
+
+	void Start () {
+        rBody = GetComponent<Rigidbody>();
+	}
+
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 dir = new Vector3(h, 0, v);
+        if(dir != Vector3.zero)
+        {
+            rBody.velocity = dir * 2;
+        }
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Wall")
+        {
+            hp--;
+            if(hp <= 0)
+            {
+                Instantiate(BombPre, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }else
+            {
+                Instantiate(HitPre, collision.contacts[0].point, Quaternion.identity);
+            }
+        }
+    }
+}
